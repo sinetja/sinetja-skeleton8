e9f553730291e28d30285acb75539ae080e4aab4
Ngoc Dao <ndao@atlassian.com>
2017-05-28 18:03:58 +1000

    Update Sinetja to 1.4.0

--- src/main/java/skeleton/App.java
+++ src/main/java/skeleton/App.java
@@ -1,19 +1,20 @@
 package skeleton;
 
+import sinetja.Action;
 import sinetja.Server;
 
 public class App {
     public static void main(String[] args) {
         Server server = new Server(8000);
 
+        Action helloAction = (req, res) -> {
+            String name = req.param("name");
+            res.respondText("Hello " + name);
+        };
+
         server
-                .GET("/", (req, res) ->
-                        res.respondText("Hello world")
-                )
-                .GET("/hello/:name", (req, res) -> {
-                    String name = req.param("name");
-                    res.respondText("Hello " + name);
-                });
+            .GET("/", (req, res) -> res.respondText("URI to Hello World: " + server.uri(helloAction, "name", "World")))
+            .GET("/hello/:name", helloAction);
 
         server.start();
         server.stopAtShutdown();
